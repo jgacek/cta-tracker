fb0b56031a98dcbad53f3c0949d05d682d89f17a
jgacek <jgacek4@gmail.com>
2026-01-10 13:32:41 -0600

    feat: Updates map_id, sends query param to output the data as json

--- tracker.py
+++ tracker.py
@@ -1,28 +1,24 @@
 import os
 import sys
 import requests
-import xml.etree.ElementTree as ET
 
 cta_train_api_key = os.getenv('CTA_TRAIN_API_KEY', None)
 cta_train_url = 'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx'
-map_id = '40380'
+# Map id for Western/Milwaukee blue line stop. Can reference full list here https://www.transitchicago.com/developers/ttdocs/#_Toc296199909
+map_id = '40670'
 
 if cta_train_api_key is None:
     print("CTA_TRAIN_API_KEY is not defined")
     sys.exit(1)
 
-params = {'key': cta_train_api_key, 'mapid': map_id}
+params = {'key': cta_train_api_key, 'mapid': map_id, 'max': 5, 'outputType': 'json'}
 
 try:
     response = requests.get(cta_train_url, params=params)
     response.raise_for_status()
-    xml_data = response.content
-    print(xml_data)
-    root = ET.fromstring(xml_data)
-    print(f"Root tag: {root.tag}")
+    json = response.json()
+    print(json)
 
 
 except requests.exceptions.RequestException as e:
     print(f"An error occurred during the request: {e}")
-except ET.ParseError as e:
-    print(f"An error occurred during XML parsing: {e}")
