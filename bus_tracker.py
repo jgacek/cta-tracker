f3bbf1988fc3daa5f622097f581a2eb67c46bc65
jgacek <jgacek4@gmail.com>
2026-01-10 15:37:07 -0600

    feat: Parses json and prints bus arrival for specific station

--- bus_tracker.py
+++ bus_tracker.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import requests
+from datetime import datetime
 
 CTA_BUS_API_KEY = os.getenv('CTA_BUS_API_KEY', None)
 CTA_BUS_URL = 'https://www.ctabustracker.com/bustime/api/v3/getpredictions'
+CTA_BUS_TIME_FORMAT = '%Y%m%d %H:%M'
+
 
 # route = '56'
 # stop_id = '18358'
@@ -19,14 +22,29 @@ def get_bus_arrivals(route, stop_id):
         response = requests.get(CTA_BUS_URL, params=params)
         response.raise_for_status()
         json = response.json()
-        print(json)
+        for bus in json.get('bustime-response').get('prd'):
+            print_bus_arrival(bus, route)
 
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred during the request: {e}")
 
 
+def print_bus_arrival(bus, route):
+    direction = bus.get('rtdir')
+    stop_name = bus.get('stpnm')
+    minutes_til_arrival = calculate_minutes_til_arrival(bus.get('prdtm'))
+    print(f"{route} {direction} arriving at {stop_name} in {minutes_til_arrival} minutes(s)")
+
+
+def calculate_minutes_til_arrival(arrival_time):
+    current_time = datetime.now()
+    formatted_arrival_time = datetime.strptime(arrival_time, CTA_BUS_TIME_FORMAT)
+    delta = formatted_arrival_time - current_time
+    return int(delta.total_seconds() / 60)
+
+
 if __name__ == "__main__":
-    map_id = int(sys.argv[1])
-    stop_id = int(sys.argv[2])
-    get_bus_arrivals(map_id, stop_id)
+    r = int(sys.argv[1])
+    stpid = int(sys.argv[2])
+    get_bus_arrivals(r, stpid)
